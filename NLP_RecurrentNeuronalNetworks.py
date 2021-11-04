868dd7b3adbadfa67cd52415a1e8e872b65fbeb7
lgramu97 <gramuglialautaro@gmail.com>
2021-11-04 19:57:27 -0300

    Predictions for recurrent neuronal netwroks (NLP)

--- NLP_RecurrentNeuronalNetworks.py
+++ NLP_RecurrentNeuronalNetworks.py
@@ -24,8 +24,8 @@ def plot_history(history):
     # list all data in history
     print(history.history.keys())
     # summarize history for accuracy
-    plt.plot(history.history['accuracy'])
-    plt.plot(history.history['val_accuracy'])
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
     plt.title('model accuracy')
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
@@ -92,10 +92,73 @@ def RNN():
     history = model.fit(train_data, train_labels, epochs=10, validation_split=0.2)
     #Plot the results
     plot_history(history)
+    # we can save the model and reload it at anytime in the future
+    model.save('imbd_RNN.h5') 
+    results = model.evaluate(test_data, test_labels)
+    print(results)
+    plot_history(history)
+
+
+def encode_text(text,word_index):
+    '''
+        Since our reviews are encoded well need to convert any review that we write into
+        that form so the network can understand it. 
+        To do that well load the encodings from the dataset and use them to encode our own data.
+    '''
+    tokens = keras.preprocessing.text.text_to_word_sequence(text)
+    tokens = [word_index[word] if word in word_index else 0 for word in tokens]
+    return sequence.pad_sequences([tokens], MAXLEN)[0]
+
+# while were at it lets make a decode function
+def decode_integers(integers,reverse_word_index):
+    PAD = 0
+    text = ""
+    for num in integers:
+      if num != PAD:
+        text += reverse_word_index[num] + " "
+    return text[:-1]
+  
+
+def test_predict():
+    word_index = imdb.get_word_index()
+    text = "that movie was just amazing, so amazing"
+    encoded = encode_text(text,word_index)
+    print(encoded)
+    reverse_word_index = {value: key for (key, value) in word_index.items()}
+    print(decode_integers(encoded,reverse_word_index))
+
+
+# now time to make a prediction
+
+def encode_decode(model,text):
+    word_index = imdb.get_word_index()
+    encoded_text = encode_text(text,word_index)
+    pred = np.zeros((1,250))
+    pred[0] = encoded_text
+    result = model.predict(pred) 
+    print(result[0])
+
+
+def predict():
+    # If prediction < 0.5 then the prediction is positive.
+    model = tf.keras.models.load_model('imbd_RNN.h5')
+    positive_review = "That movie was! really loved it and would great watch it again because it was amazingly great"
+    encode_decode(model,positive_review)
+
+    negative_review = "that movie really sucked but amazing.  I hated it and wouldn't watch it again. Was one of the worst things I've ever watched"
+    encode_decode(model,negative_review)
 
 
 def run():
-    RNN()
+    TRAIN = False
+
+    if TRAIN:
+        RNN()
+    else:
+        test_predict()
+        predict()
+
+
 
 
 if __name__ == '__main__':
